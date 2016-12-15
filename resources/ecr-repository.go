71b7fe5c6d03196d893883cb06751b452f3d2515
Florian Zeidler <f.zeidler@rebuy.com>
2016-12-15 11:10:24 +0100

    Now we nuke ECR repositories

---
+++ resources/ecr-repository.go
@@ -0,0 +1,77 @@
+package resources
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecr"
+)
+
+type ECRrepository struct {
+	svc  *ecr.ECR
+	name *string
+}
+
+func (n *ECRNuke) ListRepos() ([]Resource, error) {
+
+	var params *ecr.DescribeRepositoriesInput
+	var resp *ecr.DescribeRepositoriesOutput
+	var resources []Resource
+	var err error
+	for moreRepositories := true; moreRepositories; {
+		if resp == nil {
+			params = &ecr.DescribeRepositoriesInput{
+				MaxResults: aws.Int64(100),
+			}
+			moreRepositories = true
+		} else {
+			if resp.NextToken != nil {
+				fmt.Printf("Next token\n")
+				params = &ecr.DescribeRepositoriesInput{
+					MaxResults: aws.Int64(100),
+					NextToken:  resp.NextToken,
+				}
+				moreRepositories = true
+			} else {
+				moreRepositories = false
+				continue
+			}
+		}
+		resp, err = n.Service.DescribeRepositories(params)
+		if err != nil {
+			fmt.Printf("Error occured")
+			return nil, err
+		}
+		for _, repository := range resp.Repositories {
+			resources = append(resources, &ECRrepository{
+				svc:  n.Service,
+				name: repository.RepositoryName,
+			})
+		}
+	}
+
+	return resources, nil
+}
+
+func (r *ECRrepository) Filter() error {
+	return nil
+}
+
+func (r *ECRrepository) Remove() error {
+
+	params := &ecr.DeleteRepositoryInput{
+		RepositoryName: r.name,
+		Force:          aws.Bool(true),
+	}
+	_, err := r.svc.DeleteRepository(params)
+	return err
+}
+
+func (r *ECRrepository) Wait() error {
+
+	return nil
+}
+
+func (r *ECRrepository) String() string {
+	return fmt.Sprintf("Repository: %s", *r.name)
+}
