22f509b3755f9055ecfe846c7067aae0a74d1d3d
Sven Walter <s.walter@rebuy.com>
2018-02-25 14:04:47 +0100

    adds support for s3 object versions

--- resources/util.go
+++ resources/util.go
@@ -1,5 +1,12 @@
 package resources
 
+func UnPtrBool(ptr *bool, def bool) bool {
+	if ptr == nil {
+		return def
+	}
+	return *ptr
+}
+
 func EqualStringPtr(v1, v2 *string) bool {
 	if v1 == nil && v2 == nil {
 		return true
