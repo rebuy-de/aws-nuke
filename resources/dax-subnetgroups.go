b1773ab1da849af885bd12f754ba37b9c740d491
Thomas Vachon <tomvachon@users.noreply.github.com>
2018-03-05 09:24:04 -0500

    Add DAX Resources for deletion

---
+++ resources/dax-subnetgroups.go
@@ -0,0 +1,60 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dax"
+)
+
+type DAXSubnetGroup struct {
+	svc             *dax.DAX
+	subnetGroupName *string
+}
+
+func init() {
+	register("DAXSubnetGroup", ListDAXSubnetGroups)
+}
+
+func ListDAXSubnetGroups(sess *session.Session) ([]Resource, error) {
+	svc := dax.New(sess)
+	resources := []Resource{}
+
+	params := &dax.DescribeSubnetGroupsInput{
+		MaxResults: aws.Int64(100),
+	}
+
+	for {
+		output, err := svc.DescribeSubnetGroups(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, subnet := range output.SubnetGroups {
+			resources = append(resources, &DAXSubnetGroup{
+				svc:             svc,
+				subnetGroupName: subnet.SubnetGroupName,
+			})
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+
+		params.NextToken = output.NextToken
+	}
+
+	return resources, nil
+}
+
+func (f *DAXSubnetGroup) Remove() error {
+
+	_, err := f.svc.DeleteSubnetGroup(&dax.DeleteSubnetGroupInput{
+		SubnetGroupName: f.subnetGroupName,
+	})
+
+	return err
+}
+
+func (f *DAXSubnetGroup) String() string {
+	return *f.subnetGroupName
+}
