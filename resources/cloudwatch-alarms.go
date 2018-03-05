8ed3d0616d70f998dcb8d590ddd3036431e85313
Thomas Vachon <tomvachon@users.noreply.github.com>
2018-03-05 10:08:26 -0500

    Add Cloudwatch Dashboards and Alarms

---
+++ resources/cloudwatch-alarms.go
@@ -0,0 +1,60 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+)
+
+type CloudWatchAlarm struct {
+	svc       *cloudwatch.CloudWatch
+	alarmName *string
+}
+
+func init() {
+	register("CloudWatchAlarm", ListCloudWatchAlarms)
+}
+
+func ListCloudWatchAlarms(sess *session.Session) ([]Resource, error) {
+	svc := cloudwatch.New(sess)
+	resources := []Resource{}
+
+	params := &cloudwatch.DescribeAlarmsInput{
+		MaxRecords: aws.Int64(100),
+	}
+
+	for {
+		output, err := svc.DescribeAlarms(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, metricAlarm := range output.MetricAlarms {
+			resources = append(resources, &CloudWatchAlarm{
+				svc:       svc,
+				alarmName: metricAlarm.AlarmName,
+			})
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+
+		params.NextToken = output.NextToken
+	}
+
+	return resources, nil
+}
+
+func (f *CloudWatchAlarm) Remove() error {
+
+	_, err := f.svc.DeleteAlarms(&cloudwatch.DeleteAlarmsInput{
+		AlarmNames: []*string{f.alarmName},
+	})
+
+	return err
+}
+
+func (f *CloudWatchAlarm) String() string {
+	return *f.alarmName
+}
