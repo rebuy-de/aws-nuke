62afe64656ef006adb0e5fa732862acc403b51d0
Sven Walter <s.walter@rebuy.com>
2018-06-08 17:30:13 +0200

    Parallelise scan API requests

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,58 +1,78 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"runtime/debug"
 
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/rebuy-de/aws-nuke/pkg/awsutil"
 	"github.com/rebuy-de/aws-nuke/pkg/util"
 	"github.com/rebuy-de/aws-nuke/resources"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/sync/semaphore"
 )
 
+const ScannerParallelQueries = 16
+
 func Scan(region Region, resourceTypes []string) <-chan *Item {
-	items := make(chan *Item, 100)
+	s := &scanner{
+		items:     make(chan *Item, 100),
+		semaphore: semaphore.NewWeighted(ScannerParallelQueries),
+	}
+	go s.run(region, resourceTypes)
 
-	go func() {
-		for _, resourceType := range resourceTypes {
-			lister := resources.GetLister(resourceType)
-			rs, err := safeLister(region.Session, lister)
-			if err != nil {
-				_, ok := err.(awsutil.ErrSkipRequest)
-				if ok {
-					log.Debugf("skipping request: %v", err)
-					continue
-				}
-
-				dump := util.Indent(fmt.Sprintf("%v", err), "    !!! ")
-				log.Errorf("Listing %s failed. Please report this to https://github.com/rebuy-de/aws-nuke/issues/new.\n%s", resourceType, dump)
-				continue
-			}
-
-			for _, r := range rs {
-				items <- &Item{
-					Region:   region,
-					Resource: r,
-					State:    ItemStateNew,
-					Type:     resourceType,
-				}
-			}
-		}
-
-		close(items)
-	}()
-
-	return items
+	return s.items
 }
 
-func safeLister(sess *session.Session, lister resources.ResourceLister) (r []resources.Resource, err error) {
+type scanner struct {
+	items     chan *Item
+	semaphore *semaphore.Weighted
+}
+
+func (s *scanner) run(region Region, resourceTypes []string) {
+	ctx := context.Background()
+
+	for _, resourceType := range resourceTypes {
+		s.semaphore.Acquire(ctx, 1)
+		go s.list(region, resourceType)
+	}
+
+	// Wait for all routines to finish.
+	s.semaphore.Acquire(ctx, ScannerParallelQueries)
+
+	close(s.items)
+}
+
+func (s *scanner) list(region Region, resourceType string) {
 	defer func() {
 		if r := recover(); r != nil {
-			err = fmt.Errorf("%v\n\n%s", r.(error), string(debug.Stack()))
+			err := fmt.Errorf("%v\n\n%s", r.(error), string(debug.Stack()))
+			dump := util.Indent(fmt.Sprintf("%v", err), "    !!! ")
+			log.Errorf("Listing %s failed. Please report this to https://github.com/rebuy-de/aws-nuke/issues/new, if you think this is an application error.\n%s", resourceType, dump)
 		}
 	}()
+	defer s.semaphore.Release(1)
 
-	r, err = lister(sess)
-	return
+	lister := resources.GetLister(resourceType)
+	rs, err := lister(region.Session)
+	if err != nil {
+		_, ok := err.(awsutil.ErrSkipRequest)
+		if ok {
+			log.Debugf("skipping request: %v", err)
+			return
+		}
+
+		dump := util.Indent(fmt.Sprintf("%v", err), "    !!! ")
+		log.Errorf("Listing %s failed. Please report this to https://github.com/rebuy-de/aws-nuke/issues/new.\n%s", resourceType, dump)
+		return
+	}
+
+	for _, r := range rs {
+		s.items <- &Item{
+			Region:   region,
+			Resource: r,
+			State:    ItemStateNew,
+			Type:     resourceType,
+		}
+	}
 }
