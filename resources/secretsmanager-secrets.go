0d7709360ae459acb736f44f8da69c22fd3d01e3
Thomas Vachon <tomvachon@users.noreply.github.com>
2018-06-21 07:18:24 -0400

    Add SecretsManager cleanup
    Note: Per design of the service, secrets WILL remain recoverable (via another api call to restore) for 7 days

---
+++ resources/secretsmanager-secrets.go
@@ -0,0 +1,61 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+)
+
+type SecretsManagerSecret struct {
+	svc *secretsmanager.SecretsManager
+	ARN *string
+}
+
+func init() {
+	register("SecretsManagerSecret", ListSecretsManagerSecrets)
+}
+
+func ListSecretsManagerSecrets(sess *session.Session) ([]Resource, error) {
+	svc := secretsmanager.New(sess)
+	resources := []Resource{}
+
+	params := &secretsmanager.ListSecretsInput{
+		MaxResults: aws.Int64(100),
+	}
+
+	for {
+		output, err := svc.ListSecrets(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, secrets := range output.SecretList {
+			resources = append(resources, &SecretsManagerSecret{
+				svc: svc,
+				ARN: secrets.ARN,
+			})
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+
+		params.NextToken = output.NextToken
+	}
+
+	return resources, nil
+}
+
+func (f *SecretsManagerSecret) Remove() error {
+
+	_, err := f.svc.DeleteSecret(&secretsmanager.DeleteSecretInput{
+		SecretId:             f.ARN,
+		RecoveryWindowInDays: aws.Int64(7),
+	})
+
+	return err
+}
+
+func (f *SecretsManagerSecret) String() string {
+	return *f.ARN
+}
