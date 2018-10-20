68131a4774233a403f22a127c5b1dd0d230d15d7
Jakub Zygmunt <jakub.zygmunt@cloudreach.com>
2018-10-20 16:39:49 +0100

    Refactored IAMUserMfaAttachment to reuse ListIAMUsers. Switch to pointers for userName property

--- resources/iam-user-mfa-attachment.go
+++ resources/iam-user-mfa-attachment.go
@@ -2,6 +2,7 @@ package resources
 
 import (
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/rebuy-de/aws-nuke/pkg/types"
@@ -9,7 +10,7 @@ import (
 
 type IAMUserMfaAttachment struct {
 	svc          *iam.IAM
-	userName     string
+	userName     *string
 	serialNumber string
 }
 
@@ -20,15 +21,17 @@ func init() {
 func ListIAMUserMfaAttachments(sess *session.Session) ([]Resource, error) {
 	svc := iam.New(sess)
 
-	resp, err := svc.ListUsers(nil)
+	results, err := ListIAMUsers(sess)
 	if err != nil {
 		return nil, err
 	}
 
 	resources := make([]Resource, 0)
-	for _, user := range resp.Users {
+	for _, resource := range results {
+		user := resource.(*IAMUser)
+
 		params := &iam.ListMFADevicesInput{
-			UserName: user.UserName,
+			UserName: aws.String(user.name),
 		}
 		resp, err := svc.ListMFADevices(params)
 		if err != nil {
@@ -38,7 +41,7 @@ func ListIAMUserMfaAttachments(sess *session.Session) ([]Resource, error) {
 		for _, device := range resp.MFADevices {
 			resources = append(resources, &IAMUserMfaAttachment{
 				svc:          svc,
-				userName:     *user.UserName,
+				userName:     params.UserName,
 				serialNumber: *device.SerialNumber,
 			})
 		}
@@ -50,7 +53,7 @@ func ListIAMUserMfaAttachments(sess *session.Session) ([]Resource, error) {
 
 func (e *IAMUserMfaAttachment) Remove() error {
 	_, err := e.svc.DeactivateMFADevice(&iam.DeactivateMFADeviceInput{
-		UserName:     &e.userName,
+		UserName:     e.userName,
 		SerialNumber: &e.serialNumber,
 	})
 	if err != nil {
@@ -67,5 +70,5 @@ func (e *IAMUserMfaAttachment) Properties() types.Properties {
 }
 
 func (e *IAMUserMfaAttachment) String() string {
-	return fmt.Sprintf("%s -> %s", e.userName, e.serialNumber)
+	return fmt.Sprintf("%s -> %s", *e.userName, e.serialNumber)
 }
