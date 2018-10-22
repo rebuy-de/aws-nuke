cf4ffc6f2fb0d744855646a3ecde8d52a2f5eb9b
Jakub Zygmunt <jakub.zygmunt@cloudreach.com>
2018-10-22 12:53:44 +0200

    Refactored IAMUser MFA Attachment to use methods from IAMUser

--- resources/iam-user-mfa-attachment.go
+++ resources/iam-user-mfa-attachment.go
@@ -19,8 +19,6 @@ func init() {
 }
 
 func ListIAMUserMfaAttachments(sess *session.Session) ([]Resource, error) {
-	svc := iam.New(sess)
-
 	results, err := ListIAMUsers(sess)
 	if err != nil {
 		return nil, err
@@ -29,25 +27,39 @@ func ListIAMUserMfaAttachments(sess *session.Session) ([]Resource, error) {
 	resources := make([]Resource, 0)
 	for _, resource := range results {
 		user := resource.(*IAMUser)
-
-		params := &iam.ListMFADevicesInput{
-			UserName: aws.String(user.name),
+		userMfaDevices, err := user.ListAttachedMFADevices()
+		if err != nil {
+			return nil, err
 		}
-		resp, err := svc.ListMFADevices(params)
+		resources = append(resources, userMfaDevices...)
+	}
+
+	return resources, nil
+}
+
+func (f *IAMUser) ListAttachedMFADevices() ([]Resource, error) {
+	resources := make([]Resource, 0)
+	params := &iam.ListMFADevicesInput{
+		UserName: aws.String(f.name),
+	}
+	for {
+		resp, err := f.svc.ListMFADevices(params)
 		if err != nil {
 			return nil, err
 		}
 
 		for _, device := range resp.MFADevices {
 			resources = append(resources, &IAMUserMfaAttachment{
-				svc:          svc,
+				svc:          f.svc,
 				userName:     params.UserName,
 				serialNumber: *device.SerialNumber,
 			})
 		}
+		if params.Marker == nil {
+			break
+		}
 		params.Marker = resp.Marker
 	}
-
 	return resources, nil
 }
 
