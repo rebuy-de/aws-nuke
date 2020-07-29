6cd96958122d957d1897af3f62cba6538750a92f
Tyler Southwick <tyler.southwick@nike.com>
2020-07-29 05:39:24 -0700

    add cloudformation type support (#530)

--- go.mod
+++ go.mod
@@ -3,17 +3,16 @@ module github.com/rebuy-de/aws-nuke
 go 1.13
 
 require (
-	github.com/aws/aws-sdk-go v1.28.12
+	github.com/aws/aws-sdk-go v1.33.5
 	github.com/fatih/color v1.7.0
 	github.com/golang/mock v1.3.1
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
 	github.com/mb0/glob v0.0.0-20160210091149-1eb79d2de6c4
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
-	github.com/stretchr/testify v1.4.0
-	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17 // indirect
+	github.com/stretchr/testify v1.5.1
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	gopkg.in/yaml.v2 v2.2.2
 )
---
+++ resources/cloudformation-type_test.go
@@ -0,0 +1,66 @@
+package resources
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/golang/mock/gomock"
+	"github.com/rebuy-de/aws-nuke/mocks/mock_cloudformationiface"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCloudformationType_Remove(t *testing.T) {
+	a := assert.New(t)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockCloudformation := mock_cloudformationiface.NewMockCloudFormationAPI(ctrl)
+
+	cfnType := CloudFormationType{
+		svc: mockCloudformation,
+		typeSummary: &cloudformation.TypeSummary{
+			TypeArn: aws.String("foobar"),
+		},
+	}
+
+	mockCloudformation.EXPECT().ListTypeVersions(gomock.Eq(&cloudformation.ListTypeVersionsInput{
+		Arn: aws.String("foobar"),
+	})).Return(&cloudformation.ListTypeVersionsOutput{
+		TypeVersionSummaries: []*cloudformation.TypeVersionSummary{
+			{
+				IsDefaultVersion: aws.Bool(true),
+				VersionId:        aws.String("1"),
+				TypeName:         aws.String("t1"),
+				Type:             aws.String("RESOURCE"),
+			},
+		},
+		NextToken: aws.String("nextToken"),
+	}, nil)
+	mockCloudformation.EXPECT().ListTypeVersions(gomock.Eq(&cloudformation.ListTypeVersionsInput{
+		Arn:       aws.String("foobar"),
+		NextToken: aws.String("nextToken"),
+	})).Return(&cloudformation.ListTypeVersionsOutput{
+		TypeVersionSummaries: []*cloudformation.TypeVersionSummary{
+			{
+				IsDefaultVersion: aws.Bool(false),
+				VersionId:        aws.String("2"),
+				TypeName:         aws.String("t2"),
+				Type:             aws.String("RESOURCE"),
+			},
+		},
+	}, nil)
+
+	mockCloudformation.EXPECT().DeregisterType(&cloudformation.DeregisterTypeInput{
+		VersionId: aws.String("2"),
+		TypeName:  aws.String("t2"),
+		Type:      aws.String("RESOURCE"),
+	})
+	mockCloudformation.EXPECT().DeregisterType(&cloudformation.DeregisterTypeInput{
+		Arn: aws.String("foobar"),
+	})
+
+	err := cfnType.Remove()
+	a.Nil(err)
+
+}
