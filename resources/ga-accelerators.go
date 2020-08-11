dd8b161361067b177ff6e843fb82d5b8a9c3fdc6
Jan Michalowsky <sejamich@googlemail.com>
2020-08-11 10:31:47 +0200

    added global accelerator resources (#524)
    * added global accelerator resources
    
    * Update resources/ga-listeners.go
    
    Co-authored-by: Björn Häuser <bjoernhaeuser@gmail.com>
    
    * Update resources/ga-endpoints.go
    
    Co-authored-by: Björn Häuser <bjoernhaeuser@gmail.com>
    
    * Update resources/ga-accelerators.go
    
    Co-authored-by: Björn Häuser <bjoernhaeuser@gmail.com>
    
    * refactoring type names
    
    Co-authored-by: Björn Häuser <bjoernhaeuser@gmail.com>

---
+++ resources/ga-accelerators.go
@@ -0,0 +1,71 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/globalaccelerator"
+	"github.com/rebuy-de/aws-nuke/pkg/types"
+)
+
+// GlobalAccelerator model
+type GlobalAccelerator struct {
+	svc *globalaccelerator.GlobalAccelerator
+	ARN *string
+}
+
+func init() {
+	register("GlobalAccelerator", ListGlobalAccelerators)
+}
+
+// ListGlobalAccelerators enumerates all available accelerators
+func ListGlobalAccelerators(sess *session.Session) ([]Resource, error) {
+	svc := globalaccelerator.New(sess)
+	resources := []Resource{}
+
+	params := &globalaccelerator.ListAcceleratorsInput{
+		MaxResults: aws.Int64(100),
+	}
+
+	for {
+		output, err := svc.ListAccelerators(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, accelerator := range output.Accelerators {
+			resources = append(resources, &GlobalAccelerator{
+				svc: svc,
+				ARN: accelerator.AcceleratorArn,
+			})
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+
+		params.NextToken = output.NextToken
+	}
+
+	return resources, nil
+}
+
+// Remove resource
+func (ga *GlobalAccelerator) Remove() error {
+	_, err := ga.svc.DeleteAccelerator(&globalaccelerator.DeleteAcceleratorInput{
+		AcceleratorArn: ga.ARN,
+	})
+
+	return err
+}
+
+// Properties definition
+func (ga *GlobalAccelerator) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("ARN", ga.ARN)
+	return properties
+}
+
+// String representation
+func (ga *GlobalAccelerator) String() string {
+	return *ga.ARN
+}
---
+++ resources/ga-listeners.go
@@ -0,0 +1,98 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/globalaccelerator"
+	"github.com/rebuy-de/aws-nuke/pkg/types"
+)
+
+// GlobalAcceleratorListener model
+type GlobalAcceleratorListener struct {
+	svc *globalaccelerator.GlobalAccelerator
+	ARN *string
+}
+
+func init() {
+	register("GlobalAcceleratorListener", ListGlobalAcceleratorListeners)
+}
+
+// ListGlobalAcceleratorListeners enumerates all available listeners of all available accelerators
+func ListGlobalAcceleratorListeners(sess *session.Session) ([]Resource, error) {
+	svc := globalaccelerator.New(sess)
+	acceleratorARNs := []*string{}
+	resources := []Resource{}
+
+	// get all accelerator arns
+	acceleratorParams := &globalaccelerator.ListAcceleratorsInput{
+		MaxResults: aws.Int64(100),
+	}
+
+	for {
+		output, err := svc.ListAccelerators(acceleratorParams)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, accelerator := range output.Accelerators {
+			acceleratorARNs = append(acceleratorARNs, accelerator.AcceleratorArn)
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+
+		acceleratorParams.NextToken = output.NextToken
+	}
+
+	// get all listerners
+	for _, acceleratorARN := range acceleratorARNs {
+		params := &globalaccelerator.ListListenersInput{
+			MaxResults:     aws.Int64(100),
+			AcceleratorArn: acceleratorARN,
+		}
+
+		for {
+			output, err := svc.ListListeners(params)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, listener := range output.Listeners {
+				resources = append(resources, &GlobalAcceleratorListener{
+					svc: svc,
+					ARN: listener.ListenerArn,
+				})
+			}
+
+			if output.NextToken == nil {
+				break
+			}
+
+			params.NextToken = output.NextToken
+		}
+	}
+
+	return resources, nil
+}
+
+// Remove resource
+func (gal *GlobalAcceleratorListener) Remove() error {
+	_, err := gal.svc.DeleteListener(&globalaccelerator.DeleteListenerInput{
+		ListenerArn: gal.ARN,
+	})
+
+	return err
+}
+
+// Properties definition
+func (gal *GlobalAcceleratorListener) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("ARN", gal.ARN)
+	return properties
+}
+
+// String representation
+func (gal *GlobalAcceleratorListener) String() string {
+	return *gal.ARN
+}
