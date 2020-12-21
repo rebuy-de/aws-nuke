3228be8ce7853bdc596820a7c01107004e498fcc
Siddharth Shashikar <shashikarsidharth@gmail.com>
2020-12-22 00:30:50 +0530

    Siddharth | Enable tag based filter in secretmanager

--- resources/secretsmanager-secrets.go
+++ resources/secretsmanager-secrets.go
@@ -4,11 +4,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/rebuy-de/aws-nuke/pkg/types"
 )
 
 type SecretsManagerSecret struct {
 	svc *secretsmanager.SecretsManager
 	ARN *string
+	tags []*secretsmanager.Tag
 }
 
 func init() {
@@ -33,6 +35,7 @@ func ListSecretsManagerSecrets(sess *session.Session) ([]Resource, error) {
 			resources = append(resources, &SecretsManagerSecret{
 				svc: svc,
 				ARN: secrets.ARN,
+			    tags: secrets.Tags,
 			})
 		}
 
@@ -54,6 +57,14 @@ func (f *SecretsManagerSecret) Remove() error {
 	})
 
 	return err
+}
+
+func (f *SecretsManagerSecret) Properties() types.Properties {
+	properties := types.NewProperties()
+	for _, tagValue := range f.tags {
+		properties.SetTag(tagValue.Key, tagValue.Value)
+	}
+	return properties
 }
 
 func (f *SecretsManagerSecret) String() string {
