7c3a80f8a82a7d508029116dc09c6e5a6b875ea9
Philipp Trulson <p.trulson@rebuy.com>
2021-11-02 10:41:14 +0100

    Fix typo & filter automated neptune snapshots

--- resources/neptune-snapshots.go
+++ resources/neptune-snapshots.go
@@ -1,21 +1,27 @@
 package resources
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/neptune"
+	"github.com/rebuy-de/aws-nuke/pkg/types"
 )
 
-type NetpuneSnapshot struct {
-	svc *neptune.Neptune
-	ID  *string
+type NeptuneSnapshot struct {
+	svc          *neptune.Neptune
+	ID           *string
+	dbCluster    *string
+	snapshotType *string
 }
 
 func init() {
-	register("NetpuneSnapshot", ListNetpuneSnapshots)
+	register("NeptuneSnapshot", ListNeptuneSnapshots)
 }
 
-func ListNetpuneSnapshots(sess *session.Session) ([]Resource, error) {
+func ListNeptuneSnapshots(sess *session.Session) ([]Resource, error) {
 	svc := neptune.New(sess)
 	resources := []Resource{}
 
@@ -30,9 +36,11 @@ func ListNetpuneSnapshots(sess *session.Session) ([]Resource, error) {
 		}
 
 		for _, dbClusterSnapshot := range output.DBClusterSnapshots {
-			resources = append(resources, &NetpuneSnapshot{
-				svc: svc,
-				ID:  dbClusterSnapshot.DBClusterSnapshotIdentifier,
+			resources = append(resources, &NeptuneSnapshot{
+				svc:          svc,
+				ID:           dbClusterSnapshot.DBClusterSnapshotIdentifier,
+				dbCluster:    dbClusterSnapshot.DBClusterIdentifier,
+				snapshotType: dbClusterSnapshot.SnapshotType,
 			})
 		}
 
@@ -46,7 +54,7 @@ func ListNetpuneSnapshots(sess *session.Session) ([]Resource, error) {
 	return resources, nil
 }
 
-func (f *NetpuneSnapshot) Remove() error {
+func (f *NeptuneSnapshot) Remove() error {
 
 	_, err := f.svc.DeleteDBClusterSnapshot(&neptune.DeleteDBClusterSnapshotInput{
 		DBClusterSnapshotIdentifier: f.ID,
@@ -55,6 +63,21 @@ func (f *NetpuneSnapshot) Remove() error {
 	return err
 }
 
-func (f *NetpuneSnapshot) String() string {
+func (f *NeptuneSnapshot) String() string {
 	return *f.ID
+}
+
+func (f *NeptuneSnapshot) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("Identifier", f.ID)
+	properties.Set("DBCluster", f.dbCluster)
+
+	return properties
+}
+
+func (f *NeptuneSnapshot) Filter() error {
+	if strings.ToLower(*f.snapshotType) == "automated" {
+		return fmt.Errorf("automated snapshots can't be deleted")
+	}
+	return nil
 }
