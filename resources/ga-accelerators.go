59560b3c853405bd30a291009f9db8ebcf8155e2
Peter McNeill <peter.mcneill@sas.com>
2022-01-19 09:49:39 -0500

    Disable GlobalAccelerator before deleting it (#723)

--- resources/ga-accelerators.go
+++ resources/ga-accelerators.go
@@ -51,7 +51,22 @@ func ListGlobalAccelerators(sess *session.Session) ([]Resource, error) {
 
 // Remove resource
 func (ga *GlobalAccelerator) Remove() error {
-	_, err := ga.svc.DeleteAccelerator(&globalaccelerator.DeleteAcceleratorInput{
+	accel, err := ga.svc.DescribeAccelerator(&globalaccelerator.DescribeAcceleratorInput{
+		AcceleratorArn: ga.ARN,
+	})
+	if err != nil {
+		return err
+	}
+	if *accel.Accelerator.Enabled {
+		_, err := ga.svc.UpdateAccelerator(&globalaccelerator.UpdateAcceleratorInput{
+			AcceleratorArn: ga.ARN,
+			Enabled:        aws.Bool(false),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	_, err = ga.svc.DeleteAccelerator(&globalaccelerator.DeleteAcceleratorInput{
 		AcceleratorArn: ga.ARN,
 	})
 
