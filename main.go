d3dcb84a777ff7725d2dd04a8bc6202f471f9c29
Nelson Chen <crazysim@gmail.com>
2022-06-27 01:45:54 -0700

    Remove unused type NukeParameters in main.go (#822)
    This was probably succeeded by the one in cmd/params.go

--- main.go
+++ main.go
@@ -6,21 +6,6 @@ import (
 	"github.com/rebuy-de/aws-nuke/v2/cmd"
 )
 
-type NukeParameters struct {
-	ConfigPath string
-
-	Profile         string
-	AccessKeyID     string
-	SecretAccessKey string
-
-	NoDryRun   bool
-	Force      bool
-	ForceSleep int
-	Quiet      bool
-
-	MaxWaitRetries int
-}
-
 func main() {
 	if err := cmd.NewRootCommand().Execute(); err != nil {
 		os.Exit(-1)
