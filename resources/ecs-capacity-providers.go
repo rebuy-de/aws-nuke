4f1e4acc963bc410aab9217b5a49297961dc1aac
Octogonapus <firey45@gmail.com>
2022-11-08 10:51:44 -0500

    Add support for ECS capacity providers

---
+++ resources/ecs-capacity-providers.go
@@ -0,0 +1,60 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+)
+
+type ECSCapacityProvider struct {
+	svc *ecs.ECS
+	ARN *string
+}
+
+func init() {
+	register("ECSCapacityProvider", DescribeECSCapacityProviders)
+}
+
+func DescribeECSCapacityProviders(sess *session.Session) ([]Resource, error) {
+	svc := ecs.New(sess)
+	resources := []Resource{}
+
+	params := &ecs.DescribeCapacityProvidersInput{
+		MaxResults: aws.Int64(100),
+	}
+
+	for {
+		output, err := svc.DescribeCapacityProviders(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, capacityProviders := range output.CapacityProviders {
+			resources = append(resources, &ECSCapacityProvider{
+				svc: svc,
+				ARN: capacityProviders.CapacityProviderArn,
+			})
+		}
+
+		if output.NextToken == nil {
+			break
+		}
+
+		params.NextToken = output.NextToken
+	}
+
+	return resources, nil
+}
+
+func (f *ECSCapacityProvider) Remove() error {
+
+	_, err := f.svc.DeleteCapacityProvider(&ecs.DeleteCapacityProviderInput{
+		CapacityProvider: f.ARN,
+	})
+
+	return err
+}
+
+func (f *ECSCapacityProvider) String() string {
+	return *f.ARN
+}
