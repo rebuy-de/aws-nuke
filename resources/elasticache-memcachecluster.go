614f02fcde6aa0cc1d808fffca80f708f0083ffd
Max Krivich <maxkrivich@gmail.com>
2022-11-14 14:34:03 +0100

    Add `Properties` to `ElasticacheCacheCluster` This change will enable filtering by tags of cache clusters

--- resources/elasticache-memcachecluster.go
+++ resources/elasticache-memcachecluster.go
@@ -1,15 +1,20 @@
 package resources
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elasticache"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type ElasticacheCacheCluster struct {
 	svc       *elasticache.ElastiCache
 	clusterID *string
 	status    *string
+	tags      []*elasticache.Tag
 }
 
 func init() {
@@ -19,6 +24,15 @@ func init() {
 func ListElasticacheCacheClusters(sess *session.Session) ([]Resource, error) {
 	svc := elasticache.New(sess)
 
+	// Lookup current account ID
+	stsSvc := sts.New(sess)
+	callerID, err := stsSvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	if err != nil {
+		return nil, err
+	}
+	accountID := callerID.Account
+	region := svc.Config.Region
+
 	params := &elasticache.DescribeCacheClustersInput{MaxRecords: aws.Int64(100)}
 	resp, err := svc.DescribeCacheClusters(params)
 	if err != nil {
@@ -26,10 +40,18 @@ func ListElasticacheCacheClusters(sess *session.Session) ([]Resource, error) {
 	}
 	var resources []Resource
 	for _, cacheCluster := range resp.CacheClusters {
+		// Arn creation for listing tags
+		tags, err := svc.ListTagsForResource(&elasticache.ListTagsForResourceInput{
+			ResourceName: aws.String(fmt.Sprintf("arn:aws:elasticache:%s:%s:cluster:%s", *region, *accountID, *cacheCluster.CacheClusterId)),
+		})
+		if err != nil {
+			continue
+		}
 		resources = append(resources, &ElasticacheCacheCluster{
 			svc:       svc,
 			clusterID: cacheCluster.CacheClusterId,
 			status:    cacheCluster.CacheClusterStatus,
+			tags:      tags.TagList,
 		})
 
 	}
@@ -50,4 +72,15 @@ func (i *ElasticacheCacheCluster) Remove() error {
 
 func (i *ElasticacheCacheCluster) String() string {
 	return *i.clusterID
+}
+
+func (i *ElasticacheCacheCluster) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("Identifier", i.clusterID)
+
+	for _, tag := range i.tags {
+		properties.SetTag(tag.Key, tag.Value)
+	}
+
+	return properties
 }
