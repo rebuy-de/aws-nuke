5af13ed92e7c9a1c9b879d4ee65aed0fb08d2b4c
Thomas Lambert <thomasla@voodoo.io>
2023-01-16 10:33:05 +0100

    Add properties on multiple resources (#911)
    * feat(elbs): add CreatedTime property
    
    * feat(RDS): add CreatedTime property
    
    * feat(Redshift): add CreatedTime and tags properties
    
    * fix(s3 buckets): fix CreationDate property
    
    * feat(EMR): add CreatedTime property
    
    * feat(ECR): add tags and CreatedTime properties
    
    * feat(CloudwatchLogGroup): add CreatedTime and LastEvent properties
    
    * feat(CloudFrontDistribution): add tags and LastModifiedTime properties

--- resources/cloudfront-distributions.go
+++ resources/cloudfront-distributions.go
@@ -1,15 +1,20 @@
 package resources
 
 import (
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudfront"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type CloudFrontDistribution struct {
-	svc    *cloudfront.CloudFront
-	ID     *string
-	status *string
+	svc              *cloudfront.CloudFront
+	ID               *string
+	status           *string
+	lastModifiedTime *time.Time
+	tags             []*cloudfront.Tag
 }
 
 func init() {
@@ -29,16 +34,25 @@ func ListCloudFrontDistributions(sess *session.Session) ([]Resource, error) {
 		if err != nil {
 			return nil, err
 		}
-
 		for _, item := range resp.DistributionList.Items {
+			tagResp, err := svc.ListTagsForResource(
+				&cloudfront.ListTagsForResourceInput{
+					Resource: item.ARN,
+				})
+			if err != nil {
+				return nil, err
+			}
+
 			resources = append(resources, &CloudFrontDistribution{
-				svc:    svc,
-				ID:     item.Id,
-				status: item.Status,
+				svc:              svc,
+				ID:               item.Id,
+				status:           item.Status,
+				lastModifiedTime: item.LastModifiedTime,
+				tags:             tagResp.Tags.Items,
 			})
 		}
 
-		if *resp.DistributionList.IsTruncated == false {
+		if !*resp.DistributionList.IsTruncated {
 			break
 		}
 
@@ -46,6 +60,16 @@ func ListCloudFrontDistributions(sess *session.Session) ([]Resource, error) {
 	}
 
 	return resources, nil
+}
+
+func (f *CloudFrontDistribution) Properties() types.Properties {
+	properties := types.NewProperties().
+		Set("LastModifiedTime", f.lastModifiedTime.Format(time.RFC3339))
+
+	for _, t := range f.tags {
+		properties.SetTag(t.Key, t.Value)
+	}
+	return properties
 }
 
 func (f *CloudFrontDistribution) Remove() error {
@@ -56,6 +80,18 @@ func (f *CloudFrontDistribution) Remove() error {
 	})
 	if err != nil {
 		return err
+	}
+
+	if *resp.DistributionConfig.Enabled {
+		*resp.DistributionConfig.Enabled = false
+		_, err := f.svc.UpdateDistribution(&cloudfront.UpdateDistributionInput{
+			Id:                 f.ID,
+			DistributionConfig: resp.DistributionConfig,
+			IfMatch:            resp.ETag,
+		})
+		if err != nil {
+			return err
+		}
 	}
 
 	_, err = f.svc.DeleteDistribution(&cloudfront.DeleteDistributionInput{
