88a0a19db3e237c34abdbc62c5d8f65ad4883c78
Nicolas Pellegrin <npellegrin.dev@gmail.com>
2023-06-26 22:55:31 +0200

    Add missing pagination and run go fmt

--- resources/cloudfront-origin-access-control.go
+++ resources/cloudfront-origin-access-control.go
@@ -1,58 +1,66 @@
 package resources
 
 import (
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/cloudfront"
-  	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudfront"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
 )
 
 type CloudFrontOriginAccessControl struct {
-    svc *cloudfront.CloudFront
-    ID  *string
+	svc *cloudfront.CloudFront
+	ID  *string
 }
 
 func init() {
-    register("CloudFrontOriginAccessControl", ListCloudFrontOriginAccessControls)
+	register("CloudFrontOriginAccessControl", ListCloudFrontOriginAccessControls)
 }
 
 func ListCloudFrontOriginAccessControls(sess *session.Session) ([]Resource, error) {
-  svc := cloudfront.New(sess)
-  resources := []Resource{}
+	svc := cloudfront.New(sess)
+	resources := []Resource{}
+	params := &cloudfront.ListOriginAccessControlsInput{}
 
-  for {
-    resp, err := svc.ListOriginAccessControls(nil)
-    if err != nil {
-        return nil, err
-    }
+	for {
+		resp, err := svc.ListOriginAccessControls(params)
+		if err != nil {
+			return nil, err
+		}
 
-    for _, item := range resp.OriginAccessControlList.Items {
-      resources = append(resources,&CloudFrontOriginAccessControl{
-        svc: svc,
-        ID:  item.Id,
-      })
-    }
-    return resources, nil
-  }
+		for _, item := range resp.OriginAccessControlList.Items {
+			resources = append(resources, &CloudFrontOriginAccessControl{
+				svc: svc,
+				ID:  item.Id,
+			})
+		}
+
+		if !*resp.OriginAccessControlList.IsTruncated {
+			break
+		}
+
+		params.Marker = resp.OriginAccessControlList.NextMarker
+	}
+
+	return resources, nil
 }
 
 func (f *CloudFrontOriginAccessControl) Remove() error {
-  resp, err := f.svc.GetOriginAccessControl(&cloudfront.GetOriginAccessControlInput{
-    Id: f.ID,
-  })
-  if err != nil {
-    return err
-  }
+	resp, err := f.svc.GetOriginAccessControl(&cloudfront.GetOriginAccessControlInput{
+		Id: f.ID,
+	})
+	if err != nil {
+		return err
+	}
 
-  _, err = f.svc.DeleteOriginAccessControl(&cloudfront.DeleteOriginAccessControlInput{
-    Id: f.ID,
-    IfMatch: resp.ETag,
-  })
+	_, err = f.svc.DeleteOriginAccessControl(&cloudfront.DeleteOriginAccessControlInput{
+		Id:      f.ID,
+		IfMatch: resp.ETag,
+	})
 
-  return err
+	return err
 }
 
 func (f *CloudFrontOriginAccessControl) Properties() types.Properties {
-  properties := types.NewProperties()
-  properties.Set("ID", f.ID)
-  return properties
+	properties := types.NewProperties()
+	properties.Set("ID", f.ID)
+	return properties
 }
