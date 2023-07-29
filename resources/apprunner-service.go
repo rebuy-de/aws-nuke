118865c0a57a3e8f85fe16e2ddd568ca5ed40906
Mike Schouw <mike.schouw@gmail.com>
2023-07-29 18:47:12 +0200

    first attempt at apprunner-service

---
+++ resources/apprunner-service.go
@@ -0,0 +1,64 @@
+package resources
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
+)
+
+type AppRunnerService struct {
+	svc         *apprunner.Apprunner
+	ServiceArn  *string
+	ServiceId   *string
+	ServiceName *string
+}
+
+func init() {
+	register("AppRunnerService", ListAppRunnerServices)
+}
+
+func ListAppRunnerServices(sess *session.Session) ([]Resource, error) {
+	svc := apprunner.New(sess)
+	resources := []Resource{}
+
+	params := &apprunner.ListServicesInput{}
+
+	for {
+		resp, err := svc.ListServices(params)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, item := range resp.ServiceSummaryList {
+			resources = append(resources, &AppRunnerService{
+				svc:         svc,
+				ServiceArn:  item.ServiceArn,
+				ServiceId:   item.ServiceId,
+				ServiceName: item.ServiceName,
+			})
+		}
+
+		if resp.NextToken == nil {
+			break
+		}
+
+		params.NextToken = resp.NextToken
+	}
+
+	return resources, nil
+}
+
+func (f *AppRunnerService) Remove() error {
+	_, err := f.svc.DeleteService(&apprunner.DeleteServiceInput{
+		ServiceArn: f.ServiceArn,
+	})
+
+	return err
+}
+
+func (f *AppRunnerService) Properties() types.Properties {
+	properties := types.NewProperties()
+	properties.Set("ServiceArn", f.ServiceArn)
+	properties.Set("ServiceId", f.ServiceId)
+	properties.Set("ServiceName", f.ServiceName)
+	return properties
+}
