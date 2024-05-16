aeb28fad3e059ac66a3b7b81b006a6d8803ec2d9
Benjamin Weimer <benjamin.weimer@tngtech.com>
2024-05-16 09:54:28 +0200

    Add CreationDateTime property to DynamoDBTable

--- resources/dynamodb-tables.go
+++ resources/dynamodb-tables.go
@@ -5,12 +5,14 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
+	"time"
 )
 
 type DynamoDBTable struct {
-	svc  *dynamodb.DynamoDB
-	id   string
-	tags []*dynamodb.Tag
+	svc   *dynamodb.DynamoDB
+	id    string
+	tags  []*dynamodb.Tag
+	table *dynamodb.TableDescription
 }
 
 func init() {
@@ -27,16 +29,25 @@ func ListDynamoDBTables(sess *session.Session) ([]Resource, error) {
 
 	resources := make([]Resource, 0)
 	for _, tableName := range resp.TableNames {
-		tags, err := GetTableTags(svc, tableName)
+		describeResp, err := svc.DescribeTable(&dynamodb.DescribeTableInput{
+			TableName: aws.String(*tableName),
+		})
+
+		if err != nil {
+			continue
+		}
+
+		tags, err := GetTableTags(svc, describeResp.Table.TableArn)
 
 		if err != nil {
 			continue
 		}
 
 		resources = append(resources, &DynamoDBTable{
-			svc:  svc,
-			id:   *tableName,
-			tags: tags,
+			svc:   svc,
+			id:    *tableName,
+			tags:  tags,
+			table: describeResp.Table,
 		})
 	}
 
@@ -56,17 +67,9 @@ func (i *DynamoDBTable) Remove() error {
 	return nil
 }
 
-func GetTableTags(svc *dynamodb.DynamoDB, tableName *string) ([]*dynamodb.Tag, error) {
-	result, err := svc.DescribeTable(&dynamodb.DescribeTableInput{
-		TableName: aws.String(*tableName),
-	})
-
-	if err != nil {
-		return make([]*dynamodb.Tag, 0), err
-	}
-
+func GetTableTags(svc *dynamodb.DynamoDB, tableArn *string) ([]*dynamodb.Tag, error) {
 	tags, err := svc.ListTagsOfResource(&dynamodb.ListTagsOfResourceInput{
-		ResourceArn: result.Table.TableArn,
+		ResourceArn: tableArn,
 	})
 
 	if err != nil {
@@ -79,6 +82,7 @@ func GetTableTags(svc *dynamodb.DynamoDB, tableName *string) ([]*dynamodb.Tag, e
 func (i *DynamoDBTable) Properties() types.Properties {
 	properties := types.NewProperties()
 	properties.Set("Identifier", i.id)
+	properties.Set("CreationDateTime", i.table.CreationDateTime.Format(time.RFC3339))
 
 	for _, tag := range i.tags {
 		properties.SetTag(tag.Key, tag.Value)
